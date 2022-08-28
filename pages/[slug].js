aae3d58aa15b510220d02de4922f4350afd992e9
sohamsinh31 <sohamb2019@gmail.com>
2022-08-28 21:18:51 +0530

    2.0

--- pages/[slug].js
+++ pages/[slug].js
@@ -15,6 +15,8 @@ import Like from './Like';
 import Header from './Header';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 
 const Slug = () => {
@@ -56,6 +58,22 @@ const [useremail, setuseremail] = useState(null);
 const [displayusername, setdisplayusername] = useState(null);
 const [queries, setQueries] = useState([]);
 const [userid, setuserid] = useState('');
+const [open, setOpen] = useState(false);
+const handleOpen = () => setOpen(true);
+const handleClose = () => setOpen(false);
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'black',
+  border: '2px solid #000',
+  boxShadow: 24,
+  color:'white',
+  p: 4,
+};
 
 const colref = query(ref(rdb,'users/'),orderByChild("username"),startAt(slug2),endAt(slug2+"\uf8ff"));
 onValue(colref,(snapshot)=>{
@@ -133,22 +151,38 @@ else{
             ):(
               posts.map(({post,id},index) =>(
               post.type==''?(<p>No more images</p>):(
+                <div>
                 <ImageListItem key={index}>
                 <img
+                onMouseDown={handleClose}
+                onClick={handleOpen}
+                onTouchStart={handleOpen}
+                onTouchCancel={handleClose}
+                onMouseUp={handleOpen}
                 style={{
                   width:'120px',
                   height:'120px',
                   objectFit:'crop'
                 }}
                   src={post.imageurl}
-                  srcSet={`${post.imageurl}?w=68&h=68&fit=crop&auto=format&dpr=2 2x`}
+                  srcSet={post.imageurl}
                   alt={post.caption}
                   loading="lazy"
                 />
               </ImageListItem>
-              )
-              // <Post Type={post.type} userid={userid} displayname={displayusername}  postId={id} key={id}  userurl2={post.userurl}  username={post.username} caption = {post.caption} timestamp={post.timestamp} imageurl={post.imageurl}/>
+                  <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                   <Box sx={style}>
+              <Post Type={post.type} userid={userid} displayname={displayusername}  postId={id} key={id}  userurl2={post.userurl}  username={post.username} caption = {post.caption} timestamp={post.timestamp} imageurl={post.imageurl}/>
+              </Box>
+              </Modal>
+              </div>
           )
+              )
          ))
             }
             </ImageList>
